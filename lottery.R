bfd252f9814d1317474eb503d876f0fa45cc30a1
sylvi <sylviae@uchicago.edu>
2019-09-10 15:04:20 -0400

    edited magic numbers

--- lottery.R
+++ lottery.R
@@ -9,15 +9,18 @@
 
 library(stats)
 
+cutoff = 0.1
 arr <- array(0,10)
 for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+  arr[i] <- if (runif(1) < cuttoff) 
+    1 else 0
 }
 
 for (j in 1:100){
   arr <- array(0,10)
   for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+    arr[i] <- if (runif(1) < cuttoff) 1 else 0
   }
   print(arr)
 }
+
